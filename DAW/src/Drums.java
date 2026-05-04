565273ad3fc0e93cb803c9972da97f63a1373279
Julianna Kelley <jkelle28@gmu.edu>
2026-05-04 00:45:24 -0400

    Added snare and hat
    Still need filters on all three drums, but they're workable enough to use in class

--- DAW/src/Drums.java
+++ DAW/src/Drums.java
@@ -5,9 +5,9 @@ public final class Drums{
     private int played = 0;
     private int total = 2400;
 
-    //UNTESTED. May swap back to void & take in double[] param to alter.
+    //Needs LPF, but good enough for right now.
     public void kick(double[] mix, double gain){
-        final int numSamples = (int)(SAMPLE_RATE * 0.5);
+        final int numSamples = (int)(SAMPLE_RATE * 0.4);
         double angle1 = 0;
         double angle2 = 0;
         double angle3 = 0;
@@ -19,26 +19,51 @@ public final class Drums{
             double t = (double)idx / total;
             double freq = 45.0 + 115.0 * (1.0 - Math.min(t * 4.0, 1.0));
             angle1 += (freq / SAMPLE_RATE) * 2.0 * Math.PI;
-            angle2 += ((freq * 2) / SAMPLE_RATE) * 2.0 * Math.PI;
-            angle3 += ((freq * 3) / SAMPLE_RATE) * 2.0 * Math.PI;
+            angle2 += ((freq * 2.3) / SAMPLE_RATE) * 2.0 * Math.PI;
+            angle3 += ((freq * 3.7) / SAMPLE_RATE) * 2.0 * Math.PI;
             double click = 0;
+            double attack = Math.min(t / 0.015, 1.0);
             if(t < 0.005) click = (Math.random() * 2 - 1) * (1.0 - t / 0.005);
             double envelope1 = Math.pow(1.0 - t, 0.6);
-            double envelope2 = Math.pow(1.0 - t, 1.6);
-            double envelope3 = Math.pow(1.0 - t, 2.6);
-            mix[i] += 18000 * gain * (Math.sin(angle1) * envelope1)
-                    + (Math.sin(angle2) * envelope2 * 0.5)
-                    + (Math.sin(angle3) * envelope3 * 0.25) 
-                    + (click * 0.3);
+            double envelope2 = Math.pow(1.0 - t, 3.0);
+            double envelope3 = Math.pow(1.0 - t, 5.0);
+            mix[i] += 18000 * gain * attack * ((Math.sin(angle1) * envelope1)
+                    + (Math.sin(angle2) * envelope2 * 0.25)
+                    + (Math.sin(angle3) * envelope3 * 0.15) 
+                    + (click * 0.003));
         }
         played += n;
     }
 
-    public void snare(){
-
+    //HPF? Fiddle with variance some more
+    public void snare(double[] mix, double gain){
+        int n = Math.min(mix.length, total - played);
+        for (int i = 0; i < n; i++) {
+            int idx = played + i;
+            double sample1 = Math.random() * 14000 - 7000;
+            double sample2 = Math.random() * 14000 - 6500;
+            double sample3 = Math.random() * 14000 - 7500;        
+            double envelope1 = (double)(total - idx) / total;
+            double envelope2 = ((double)(total - idx) / total) * 0.5;
+            double envelope3 = ((double)(total - idx) / total) * 0.3;
+            mix[i] += ((sample1 * envelope1) + (sample2 * envelope2) + (sample3 * envelope3)) * gain;
+        }
+        played += n;
     }
 
-    public void hat(){
-
+    //HPF? Fiddle with variance & equations some more.
+    public void hat(double[] mix, double gain){
+        int n = Math.min(mix.length, total - played);
+        for (int i = 0; i < n; i++) {
+            int idx = played + i;
+            double sample1 = Math.random() * 10000 - 5000;
+            double sample2 = Math.random() * 10000 - 4500;
+            double sample3 = Math.random() * 10000 - 5500;        
+            double envelope1 = Math.pow((double)(total - idx) / total, 2);
+            double envelope2 = Math.pow((double)(total - idx) / total, 2.5);
+            double envelope3 = Math.pow((double)(total - idx) / total, 3);
+            mix[i] += ((sample1 * envelope1) + (sample2 * envelope2) + (sample3 * envelope3)) * gain;
+        }
+        played += n;
     }
 }
